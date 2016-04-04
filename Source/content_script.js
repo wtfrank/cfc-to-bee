2f6c671a5edade09fc70a7eecdd365c6b9b4631e
Frankie Fisher <frankie@terrorise.me.uk>
2016-04-04 17:44:48 +0100

    initial conversion from cloud to cfc

--- Source/content_script.js
+++ Source/content_script.js
@@ -7,10 +7,7 @@ function walk(node)
 	
 	var child, next;
 	
-	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
-	    || node.classList.indexOf('ace_editor') > -1) {
-		return;
-	}
+
 
 	switch ( node.nodeType )  
 	{
@@ -27,7 +24,9 @@ function walk(node)
 			break;
 
 		case 3: // Text node
+                        if(node.parentElement.tagName.toLowerCase() != "script") {
 			handleText(node);
+                        }
 			break;
 	}
 }
@@ -36,11 +35,49 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bThe Imperium\b/g, "Those Bee guys");
+	v = v.replace(/\bThe imperium\b/g, "Those bee guys");
+	v = v.replace(/\bthe Imperium\b/g, "those bee guys");
+	v = v.replace(/\bthe imperium\b/g, "those bee guys");a
+
+	v = v.replace(/\bThe&nbsp;Imperium\b/g, "Those Bee guys");
+
+	v = v.replace(/\bThe CFC\b/g, "Those Bee guys");
+	v = v.replace(/\bThe cfc\b/g, "Those bee guys");
+	v = v.replace(/\bthe CFC\b/g, "those bee guys");
+	v = v.replace(/\bthe cfc\b/g, "those bee guys");
 	
+	v = v.replace(/\bThe Cluster Fuck Coalition\b/g, "Those Bee guys");
+	v = v.replace(/\bThe ClusterFuck Coalition\b/g, "Those Bee guys");
+	v = v.replace(/\bThe Cluster fuck Coalition\b/g, "Those Bee guys");
+	v = v.replace(/\bThe Clusterfuck Coalition\b/g, "Those Bee guys");
+	v = v.replace(/\bThe cluster fuck coalition\b/g, "Those bee guys");
+	v = v.replace(/\bThe clusterfuck coalition\b/g, "Those bee guys");
+	v = v.replace(/\bthe Cluster fuck coalition\b/g, "those bee guys");
+	v = v.replace(/\bthe Clusterfuck coalition\b/g, "those bee guys");
+	v = v.replace(/\bthe cluster fuck Coalition\b/g, "those bee guys");
+	v = v.replace(/\bthe clusterfuck Coalition\b/g, "those bee guys");
+	v = v.replace(/\bthe cluster fuck coalition\b/g, "those bee guys");
+	v = v.replace(/\bthe clusterfuck coalition\b/g, "those bee guys");
+
+        v = v.replace(/\bAn Imperium\b/g, "Those Bee guys");
+        v = v.replace(/\bAn imperium\b/g, "Those bee guys");
+        v = v.replace(/\ban Imperium\b/g, "those bee guys");
+        v = v.replace(/\ban imperium\b/g, "those bee guys");
+
+        v = v.replace(/\bImperium\b/g, "Bee guys");
+        v = v.replace(/\bimperium\b/g, "bee guys");
+
+        v = v.replace(/\bDreddit is recruiting\b/g, "Dreddit is no longer recruiting");
+        v = v.replace(/\bdreddit is recruiting\b/g, "dreddit is no longer recruiting");
+
+        v = v.replace(/\bCFC\b/g, "Bee guys");
+        v = v.replace(/\bCfc\b/g, "Bee guys");
+        v = v.replace(/\bcfc\b/g, "bee guys");
+
+        v = v.replace(/\bGoons\b/g, "Bees");
+        v = v.replace(/\bgoons\b/g, "bees");
+
 	textNode.nodeValue = v;
 }
 
