cf72bb2991adce7b0cb59654166134756a2de7b5
Jan Finkenzeller <finkenzellerjan@gmail.com>
2016-04-04 21:21:06 +0200

    Replace alle casings for the different strings
    Avoids tricking the Extension by writing something like "cFC"

--- Source/content_script.js
+++ Source/content_script.js
@@ -35,49 +35,24 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Imperium\b/g, "Those Bee guys");
-	v = v.replace(/\bThe imperium\b/g, "Those bee guys");
-	v = v.replace(/\bthe Imperium\b/g, "those bee guys");
-	v = v.replace(/\bthe imperium\b/g, "those bee guys");
+	v = v.replace(/\bthe imperium\b/gi, "those bee guys");
 
-	v = v.replace(/\bThe&nbsp;Imperium\b/g, "Those Bee guys");
+	v = v.replace(/\bThe&nbsp;Imperium\b/gi, "Those Bee guys");
 
-	v = v.replace(/\bThe CFC\b/g, "Those Bee guys");
-	v = v.replace(/\bThe cfc\b/g, "Those bee guys");
-	v = v.replace(/\bthe CFC\b/g, "those bee guys");
-	v = v.replace(/\bthe cfc\b/g, "those bee guys");
+	v = v.replace(/\bthe cfc\b/gi, "those bee guys");
 	
-	v = v.replace(/\bThe Cluster Fuck Coalition\b/g, "Those Bee guys");
-	v = v.replace(/\bThe ClusterFuck Coalition\b/g, "Those Bee guys");
-	v = v.replace(/\bThe Cluster fuck Coalition\b/g, "Those Bee guys");
-	v = v.replace(/\bThe Clusterfuck Coalition\b/g, "Those Bee guys");
-	v = v.replace(/\bThe cluster fuck coalition\b/g, "Those bee guys");
-	v = v.replace(/\bThe clusterfuck coalition\b/g, "Those bee guys");
-	v = v.replace(/\bthe Cluster fuck coalition\b/g, "those bee guys");
-	v = v.replace(/\bthe Clusterfuck coalition\b/g, "those bee guys");
-	v = v.replace(/\bthe cluster fuck Coalition\b/g, "those bee guys");
-	v = v.replace(/\bthe clusterfuck Coalition\b/g, "those bee guys");
-	v = v.replace(/\bthe cluster fuck coalition\b/g, "those bee guys");
-	v = v.replace(/\bthe clusterfuck coalition\b/g, "those bee guys");
+	v = v.replace(/\bthe cluster fuck coalition\b/gi, "those bee guys");
+	v = v.replace(/\bthe clusterfuck coalition\b/gi, "those bee guys");
 
-        v = v.replace(/\bAn Imperium\b/g, "Those Bee guys");
-        v = v.replace(/\bAn imperium\b/g, "Those bee guys");
-        v = v.replace(/\ban Imperium\b/g, "those bee guys");
-        v = v.replace(/\ban imperium\b/g, "those bee guys");
+        v = v.replace(/\ban imperium\b/gi, "those bee guys");
 
-        v = v.replace(/\bImperium\b/g, "Bee guys");
-        v = v.replace(/\bimperium\b/g, "bee guys");
+        v = v.replace(/\bimperium\b/gi, "bee guys");
 
-        v = v.replace(/\bDreddit is Recruiting\b/g, "Dreddit is no longer Recruiting");
-        v = v.replace(/\bDreddit is recruiting\b/g, "Dreddit is no longer recruiting");
-        v = v.replace(/\bdreddit is recruiting\b/g, "dreddit is no longer recruiting");
+        v = v.replace(/\bdreddit is recruiting\b/gi, "dreddit is no longer recruiting");
 
-        v = v.replace(/\bCFC\b/g, "Bee guys");
-        v = v.replace(/\bCfc\b/g, "Bee guys");
-        v = v.replace(/\bcfc\b/g, "bee guys");
+        v = v.replace(/\bcfc\b/gi, "bee guys");
 
-        v = v.replace(/\bGoons\b/g, "Bees");
-        v = v.replace(/\bgoons\b/g, "bees");
+        v = v.replace(/\bgoons\b/gi, "bees");
 
 	textNode.nodeValue = v;
 }
